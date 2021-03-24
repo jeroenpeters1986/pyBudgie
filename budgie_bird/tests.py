4e2167ff1d3c9370b15589e528c4a961298bc48f
Jeroen Peters <jeroenpeters1986@gmail.com>
2021-03-24 08:47:34 +0100

    precommit test

--- budgie_bird/tests.py
+++ budgie_bird/tests.py
@@ -5,11 +5,14 @@ from budgie_user.models import BudgieUser
 
 
 class BreederModelTest(TestCase):
-
     def setUp(self):
 
-        self.app_user = BudgieUser.objects.create(username='henk', breeding_reg_nr="OMG1337")
-        self.other_user = BudgieUser.objects.create(username='cees', breeding_reg_nr="OhNo666")
+        self.app_user = BudgieUser.objects.create(
+            username="henk", breeding_reg_nr="OMG1337"
+        )
+        self.other_user = BudgieUser.objects.create(
+            username="cees", breeding_reg_nr="OhNo666"
+        )
 
         self.breeder1 = Breeder.objects.create(
             user=self.app_user,
