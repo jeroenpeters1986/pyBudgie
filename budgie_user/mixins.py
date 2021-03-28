a8b9c3a97a36241c45398f4a8a6a1ee2a4744a97
Jeroen Peters <jeroenpeters1986@gmail.com>
2021-03-28 11:53:30 +0200

    #6: restrict Django Admin to users own objects, also for manytomanyform

--- budgie_user/mixins.py
+++ budgie_user/mixins.py
@@ -1,3 +1,6 @@
+from budgie_bird.models import ColorProperty
+
+
 class BudgieUserMixin:
     def save_model(self, request, obj, form, change):
         # For non-superusers, always save as own user instance
@@ -11,3 +14,11 @@ class BudgieUserMixin:
         if not request.user.is_superuser:
             return queryset.filter(user=request.user)
         return queryset
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        # For non-superusers, always show own instances
+        if not request.user.is_superuser:
+            if db_field.name in ["color_property", "split_property"]:
+                kwargs["queryset"] = ColorProperty.objects.filter(user=request.user)
+
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
