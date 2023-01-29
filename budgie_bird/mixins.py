c741ec1352943971ebe9e4345fe4925cee40f423
Jeroen Peters <jeroenpeters1986@gmail.com>
2023-01-29 11:47:44 +0100

    New export functionality + colors

--- budgie_bird/mixins.py
+++ budgie_bird/mixins.py
@@ -20,4 +20,24 @@ class AdminExportCsvMixin:
 
         return response
 
-    export_as_csv.short_description = _("Export selected as .csv")
+    export_as_csv.short_description = _("Export selected to .csv")
+
+
+class AdminExportAllCsvMixin:
+    def export_all_as_csv(self, request, queryset):
+        """Export all objects for a model to CSV"""
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in self.model.objects.filter(user=request.user):
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_all_as_csv.short_description = _("Export all to .csv")
